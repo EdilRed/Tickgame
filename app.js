8679c516b0954cd9f0a85cc82a2d4f29fe8627f0
Wisarut Tiyamanee <jtiyamanee@hotmail.com>
2020-04-11 01:33:21 +0700

    init app server

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
+const path = require('path');
 const express = require("express");
 const morgan = require("morgan");
+
+const app = express();
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
+app.use(express.json({
+    limit: '10kb'
+}));
+
+// ROUTES
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'error',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
+    throw new Error(`Can't find ${req.originalUrl} on this server!`);
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: './config.env'
+});
+
+const app = require('./app');
+
+const SERVER_PORT = process.env.PORT || 6000;
+const MONGODB_URI = process.env.DATABASE;
+const MONGODB_OPTIONS = {
+    dbName: 'Chat-app',
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+};
+
+// Connect to mongoDB
+mongoose
+    .connect(MONGODB_URI, MONGODB_OPTIONS)
+    .then(() => console.log('DB connection successful!'))
+    .catch((err) => {
+        throw new Error(err.message);
+    });
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB error: ', err);
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`App running on port ${SERVER_PORT}...`);
+});
