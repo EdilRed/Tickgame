98dbe02f245f2b662c4eb13074f90fb27ea040a2
Wisarut Tiyamanee <jtiyamanee@hotmail.com>
2020-04-18 12:47:52 +0700

    Import sidebar bootstrap

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 const path = require('path');
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
 const questionRouter = require('./routes/questionRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Implement CORS
+app.use(cors());
+
+app.options('*', cors());
+
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Development logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-};
+  app.use(morgan('dev'));
+}
 
-app.use(express.json({
-    limit: '10kb'
-}));
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
 
 // ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/users', userRouter);
 app.use('/api/questions', questionRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'error',
-        message: `Can't find ${req.originalUrl} on this server!`
-    })
-    throw new Error(`Can't find ${req.originalUrl} on this server!`);
+  res.status(404).json({
+    status: 'error',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  throw new Error(`Can't find ${req.originalUrl} on this server!`);
 });
 
 module.exports = app;
